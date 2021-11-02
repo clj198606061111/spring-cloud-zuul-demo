0a1c382d265e98d514e407b9a44e2bfe29adec61
clj198606061111@163.com <clj-1986>
2021-11-02 23:33:45 +0800

    文件上传测试

--- server-one/src/main/java/com/itclj/controller/ItcljController.java
+++ server-one/src/main/java/com/itclj/controller/ItcljController.java
@@ -1,16 +1,38 @@
 package com.itclj.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 
 @RestController
 @RequestMapping("/itclj")
 public class ItcljController {
 
+    private Logger logger = LoggerFactory.getLogger(ItcljController.class);
+
     @GetMapping("/{id}")
     public String getById(@PathVariable("id") String id) {
         return "Id is " + id;
+    }
+
+    @PostMapping("/upload")
+    public String upload(@RequestParam("file") MultipartFile file) throws UnsupportedEncodingException {
+        String fileName = file.getOriginalFilename();
+        logger.info(new String(fileName.getBytes(),"UTF-8"));
+        String filePath = "/itclj/";
+        File dest = new File(filePath + fileName);
+        try {
+            file.transferTo(dest);
+            logger.info("上传成功,filename={}", fileName);
+            return "上传成功";
+        } catch (IOException e) {
+            logger.error(e.toString(), e);
+        }
+        return "上传失败！";
     }
 }
